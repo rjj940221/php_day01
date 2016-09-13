39d74cdcc269ad7ee93d7f1c0e82e337e66ca810
rjj940221 <94.rob.jones@gmail.com>
2016-09-13 22:23:19 +0200

    sort

--- ex14/agent_stats.php
+++ ex14/agent_stats.php
@@ -3,7 +3,8 @@
 $num = 0;
 $val = 0;
 $re = 0;
-$data = array();
+$data = array(array());
+$line;
 function ft_sort($tab)
 {
 	$size=count($tab);
@@ -11,7 +12,12 @@ function ft_sort($tab)
 	{
 		for($j = $i + 1; $j < $size; $j++)
 		{
-			if(strcasecmp($tab[$i][0], $tab[$j][0]) >0 )
+			echo "$i to $j comper ";
+			echo $tab[$i][0];
+			echo " ";
+			echo $tab[$j][0];
+			echo "\n";
+			if(strcasecmp($tab[$i][0], $tab[$j][0]) > 0)
 			{
 				$temp = $tab[$i];
 				$tab[$i] = $tab[$j];
@@ -19,34 +25,49 @@ function ft_sort($tab)
 			}
 		}
 	}
+	return ($tab);
 }
-while(! feof(STDIN))
+if ($argc == 2)
 {
-	array_push($data, fgetcsv(STDIN,100,";"));
-	if ($data[$num][1])
+	while(! feof(STDIN))
 	{
-		$re+=$data[$num][1];
+		if ($num)
+			$line = fgetcsv(STDIN,1000,";");
+		else
+			fgetcsv(STDIN,1000,";");
+		if ($num && $line[1])
+		{
+			$re+=$line[1];
+			$val++;
+		}
+		if ($num)
+		{
+			$data[$num -1][0] = $line[0];
+			$data[$num -1][1] = $line[1];
+			$data[$num -1][2] = $line[2];
+			$data[$num -1][3] = $line[3];
+		}
 		$num++;
-		$val++;
 	}
-}
-if (strcmp($argv[1], "average")==false)
-	echo $re / $val . "\n";
-if (strcasecmp($argv[1], "average_user"))
-{
-	ft_sort($data);
-	foreach ($data as $elm) {
-		if($elm[1] != false && $elm[3] !=false){
-			echo $elm[0] . ($elm[1] + $elm[3]) / 2 ."\n";
+	if (strcmp($argv[1], "average")==false)
+		echo $re / $val . "\n";
+	if (strcasecmp($argv[1], "average_user") == false)
+	{
+		$data = ft_sort($data);
+		foreach ($data as $elm) {
+			if($elm[1] != false && $elm[3] !=false){
+				echo $elm[0] ." ". ($elm[1] + $elm[3]) / 2 ."\n";
+			}
 		}
 	}
-}
-if (strcasecmp($argv[1], "moulinette_variance")) {
-	
-	ft_sort($data);
-	foreach ($data as $elm) {
-		if($elm[1] != false && $elm[3] !=false){
-			echo $elm[0] . ($elm[1] - $elm[3]) . "\n";
+	if (strcasecmp($argv[1], "moulinette_variance") == false) 
+	{
+
+		$data = ft_sort($data);
+		foreach ($data as $elm) {
+			if($elm[1] != false && $elm[3] !=false){
+				echo $elm[0] . " " . ($elm[1] - $elm[3]) . "\n";
+			}
 		}
 	}
 }
