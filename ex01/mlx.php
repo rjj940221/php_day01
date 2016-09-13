a2163b40f3d63433a49f93987bd7a6f16786195e
Robert JONES <rojones@e5r10p5.jnb.42.fr>
2016-09-13 17:20:00 +0200

    do op2 and fix X

--- ex01/mlx.php
+++ ex01/mlx.php
@@ -1,9 +1,10 @@
 #!/usr/bin/php
 <?PHP
-for($i=0;$i<10;$i++)
+for($i=1;$i<=1000;$i++)
 {
-	for($l=0;$l<100;$l++)
-		echo X;
-	echo "\n";
+	echo X;
+	if($i%82==0)
+		echo "\n";
 }
+echo "\n";
 ?>
--- ex11/do_op_2.php
+++ ex11/do_op_2.php
@@ -5,5 +5,91 @@
 	echo "Incorrect Parameters\n";
 	exit (0);
  }
- if (strchr($argv[1]
+ if (strchr($argv[1], "+"))
+ {
+	 $vars = explode("+", $argv[1]);
+	 if (count($vars) != 2)
+	 {
+		 echo "Syntax Error wrong number of terms\n";
+		 exit (0);
+	 }
+	 $vars[0] = trim($vars[0], " ");
+	 $vars[1] = trim($vars[1], " ");
+	 if (is_numeric($vars[0]) == false || is_numeric($vars[1]) == false)
+	 {
+		 echo "Syntax Error\n";
+		 exit (0);
+	 }
+	 echo $vars[0] + $vars[1] . "\n";
+ }
+ elseif (strchr($argv[1], "-"))
+ {
+	 $vars = explode("-", $argv[1]);
+	 if (count($vars) != 2)
+	 {
+		 echo "Syntax Error wrong number of terms\n";
+		 exit (0);
+	 }
+	 $vars[0] = trim($vars[0], " ");
+	 $vars[1] = trim($vars[1], " ");
+	 if (is_numeric($vars[0]) == false || is_numeric($vars[1]) == false)
+	 {
+		 echo "Syntax Error\n";
+		 exit (0);
+	 }
+	 echo $vars[0] - $vars[1] . "\n";
+ }
+ elseif (strchr($argv[1], "*"))
+ {
+	 $vars = explode("*", $argv[1]);
+	 if (count($vars) != 2)
+	 {
+		 echo "Syntax Error wrong number of terms\n";
+		 exit (0);
+	 }
+	 $vars[0] = trim($vars[0], " ");
+	 $vars[1] = trim($vars[1], " ");
+	 if (is_numeric($vars[0]) == false || is_numeric($vars[1]) == false)
+	 {
+		 echo "Syntax Error\n";
+		 exit (0);
+	 }
+	 echo $vars[0] * $vars[1] . "\n";
+ }
+ elseif (strchr($argv[1], "/"))
+ {
+	 $vars = explode("/", $argv[1]);
+	 if (count($vars) != 2)
+	 {
+		 echo "Syntax Error wrong number of terms\n";
+		 exit (0);
+	 }
+	 $vars[0] = trim($vars[0], " ");
+	 $vars[1] = trim($vars[1], " ");
+	 if (is_numeric($vars[0]) == false || is_numeric($vars[1]) == false)
+	 {
+		 echo "Syntax Error\n";
+		 exit (0);
+	 }
+	 echo $vars[0] / $vars[1] . "\n";
+ }
+ elseif (strchr($argv[1], "%"))
+ {
+	 $vars = explode("%", $argv[1]);
+	 if (count($vars) != 2)
+	 {
+		 echo "Syntax Error wrong number of terms\n";
+		 exit (0);
+	 }
+	 $vars[0] = trim($vars[0], " ");
+	 $vars[1] = trim($vars[1], " ");
+	 if (is_numeric($vars[0]) == false || is_numeric($vars[1]) == false)
+	 {
+		 echo "Syntax Error\n";
+		 exit (0);
+	 }
+	 echo $vars[0] % $vars[1] . "\n";
+ }
+ else
+	 echo "Syntax Error\n";
 ?>
